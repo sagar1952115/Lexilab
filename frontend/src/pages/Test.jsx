ee4453f3fc27ba63972acf5e0108cb1b3c05bed2
Sagar Kumar <sagarkumar1952115@gmail.com>
2023-09-23 00:25:46 +0530

    added loader with every api call

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -6,6 +6,7 @@ import { useLocation } from "react-router-dom";
 
 const Test = () => {
   const { state } = useLocation();
+  const [loading, setLoading] = useState(false);
 
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
@@ -15,6 +16,7 @@ const Test = () => {
     // eslint-disable-next-line
   }, [questions]);
   const fetchQuestion = async () => {
+    setLoading(true);
     try {
       const res = await axios.get(
         `https://lexilab.onrender.com/api/question?lang_id=${
@@ -25,16 +27,23 @@ const Test = () => {
     } catch (err) {
       console.log(err);
     }
+    setLoading(false);
   };
   return (
     <div>
-      {questions.length !== 0 && (
-        <TestPage
-          id={state.id}
-          lang={state.lang}
-          level={state.level}
-          questions={questions}
-        />
+      {loading ? (
+        <div className="w-24 h-24 mx-auto my-5 border-t-2 border-blue-500 rounded-full animate-spin"></div>
+      ) : (
+        <div>
+          {questions.length !== 0 && (
+            <TestPage
+              id={state.id}
+              lang={state.lang}
+              level={state.level}
+              questions={questions}
+            />
+          )}
+        </div>
       )}
     </div>
   );
